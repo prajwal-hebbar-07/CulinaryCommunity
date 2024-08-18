14d451da91e33e56872350ba02fd34d4bc9dc152
Hebbar <prajwalhebbaras@gmail.com>
2024-08-18 17:47:00 +0530

    feat(community): add header, explanation, and perks content with image
        - Added a header to the community page to introduce the section.
        - Included a brief explanation to provide context for the community page.
        - Highlighted community perks with an accompanying image to enhance visual appeal.

--- src/app/community/page.js
+++ src/app/community/page.js
@@ -1,3 +1,27 @@
+import Image from "next/image";
+
+import logo from "@/assets/logo.png";
+
 export default function MealCommunityPage() {
-  return <h1 className="text-3xl font-bold">Meal Community</h1>;
+  return (
+    <div className="text-center my-16">
+      <div className="flex flex-row justify-center items-center">
+        <h3 className="text-6xl text-slate-300/75 font-bold me-4">
+          One Shared Passion:
+        </h3>
+        <h2 className="text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-amber-300 to-orange-600">
+          Food
+        </h2>
+      </div>
+      <p className="text-lg bg-clip-text text-transparent bg-gradient-to-r from-amber-300 to-orange-600 my-10 font-light">
+        Join our community to have a complete culinary experience!
+      </p>
+      <div className="flex flex-col items-center gap-16">
+        <h3 className="text-4xl font-bold text-amber-200/75 mt-8">
+          Community Perks
+        </h3>
+        <Image src={logo} width={150} height="auto"></Image>
+      </div>
+    </div>
+  );
 }
