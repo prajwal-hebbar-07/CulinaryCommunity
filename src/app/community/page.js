fca45b0139f3790d1bb41f36399364683bd9eaf5
Hebbar <prajwalhebbaras@gmail.com>
2024-08-18 19:46:03 +0530

    feat(community): complete community page with final content and styling
        - Finalized the community page by adding all remaining content and necessary styling.
        - Ensured the layout is consistent with the rest of the site, providing a polished user experience.

--- src/app/community/page.js
+++ src/app/community/page.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
 import logo from "@/assets/logo.png";
+import pizza from "@/assets/pizza.jpg";
+import tomato from "@/assets/tomato-salad.jpg";
 
 export default function MealCommunityPage() {
   return (
@@ -21,6 +23,28 @@ export default function MealCommunityPage() {
           Community Perks
         </h3>
         <Image src={logo} width={150} height="auto"></Image>
+      </div>
+      <div className="flex flex-col items-center gap-16">
+        <h3 className="text-2xl font-bold text-amber-200/75 mt-8">
+          Share and discover recepies
+        </h3>
+        <Image
+          src={pizza}
+          width={150}
+          height="auto"
+          className="rounded-full"
+        ></Image>
+      </div>
+      <div className="flex flex-col items-center gap-16">
+        <h3 className="text-2xl font-bold text-amber-200/75 mt-8">
+          Find new friends and like minded people
+        </h3>
+        <Image
+          src={tomato}
+          width={150}
+          height="auto"
+          className="rounded-full"
+        ></Image>
       </div>
     </div>
   );
