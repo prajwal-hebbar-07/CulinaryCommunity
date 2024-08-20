8c032dab662deb51c24a38789fcc0ef8196939ec
Hebbar <prajwalhebbaras@gmail.com>
2024-08-20 10:46:45 +0530

    feat(ui): add a back button to not found page of meals page

--- src/app/meals/not-found.js
+++ src/app/meals/not-found.js
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function MealsNotFound() {
   return (
     <div className="flex items-center justify-center">
       <h1 className="text-center font-bold text-3xl">Meals Page Not Found!</h1>
       <p className="text-center">Try some other meal</p>
+      <Link href="..">Go Back</Link>
     </div>
   );
 }
