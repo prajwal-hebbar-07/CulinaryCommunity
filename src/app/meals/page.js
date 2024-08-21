ee3d21426c458073ebd035ff1d582bcc8cba884e
Hebbar <prajwalhebbaras@gmail.com>
2024-08-21 18:36:19 +0530

    feat(ui): add global loading spinner and MealsComponent with suspense handling
        - Added a loading spinner to the `loading.js` file in the root app directory to handle global loading states.
        - Created a `MealsComponent` on the Meals page to manage asynchronous data fetching.
        - Utilized React's `Suspense` to display the loading state while fetching meals data, ensuring a smooth user experience.

--- src/app/meals/page.js
+++ src/app/meals/page.js
@@ -1,7 +1,15 @@
+import { Suspense } from "react";
+
 import MealsGrid from "@/components/meals/meals-grid";
 import MealsHero from "@/components/meals/meals-hero";
 
 import { getMeals } from "@/lib/meals";
+
+async function MealsComponent() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
 
 export default async function MealsPage() {
   const meals = await getMeals();
@@ -11,7 +19,9 @@ export default async function MealsPage() {
         <MealsHero />
       </section>
       <section className="mt-16 mb-8 mx-16">
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<div>Loading Meals...</div>}>
+          <MealsComponent />
+        </Suspense>
       </section>
     </div>
   );
